993b61f9fd70323cd845681dcfceedeb1981c1b1
Marcelo Silva <desenv.marcelo.silva@gmail.com>
2022-05-19 16:28:00 -0300

    refactor: router to own module.
    This make remove and refactor easier.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { RegisterComponent } from 'src/app/auth/components/register/register.component';
-
-const routes: Routes = [
-  {
-    path: 'register',
-    component: RegisterComponent,
-  },
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
