2096c1bf0a7f42ae687be1b870252dd94722ca1a
Marcelo Silva <desenv.marcelo.silva@gmail.com>
2022-05-19 16:58:31 -0300

    feat: add behavior to form (wip)

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,8 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'mc-register',
   templateUrl: 'register.component.html',
   styleUrls: ['./register.component.scss'],
 })
-export class RegisterComponent {}
+export class RegisterComponent implements OnInit {
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  initializeForm(): void {
+    console.log('initialize');
+    this.form = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+      email: ['', Validators.email],
+    });
+  }
+
+  onSubmit(): void {
+    console.log('submit', this.form.value, 'valid: ', this.form.valid);
+  }
+}
