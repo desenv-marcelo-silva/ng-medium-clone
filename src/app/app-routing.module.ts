3ae790bcef9927fccf280e0a0b5ace25683a45e6
Marcelo Silva <desenv.marcelo.silva@gmail.com>
2022-05-19 16:22:37 -0300

    feat: create and use register component and route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { RegisterComponent } from 'src/app/auth/components/register/register.component';
+
+const routes: Routes = [
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
