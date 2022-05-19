1f2e0b946b4601d5c1997552ebb2e72a7ce1e310
Marcelo Silva <desenv.marcelo.silva@gmail.com>
2022-05-19 17:34:24 -0300

    feat: configure devtools
    Need install chrome redux dev tools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { AppRoutingModule } from 'src/app/app-routing.module';
 import { AppComponent } from 'src/app/app.component';
 import { AuthModule } from 'src/app/auth/auth.module';
+
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
@@ -13,6 +16,10 @@ import { AuthModule } from 'src/app/auth/auth.module';
     AppRoutingModule,
     AuthModule,
     StoreModule.forRoot([]),
+    StoreDevtoolsModule.instrument({
+      maxAge: 10,
+      logOnly: environment.production,
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent],
