96a4801d34e34adeca2dcfb5a909f9b23eca1579
Marcelo Silva <desenv.marcelo.silva@gmail.com>
2022-07-06 20:41:20 -0300

    feat: navigate to home after successfully register

--- src/app/auth/store/effects/register.effect.ts
+++ src/app/auth/store/effects/register.effect.ts
@@ -5,19 +5,21 @@ import {
   registerFailureAction,
   registerSuccessAction,
 } from 'src/app/auth/store/actions/register.action';
-import { switchMap, map, catchError } from 'rxjs/operators';
+import { switchMap, map, catchError, tap } from 'rxjs/operators';
 import { of } from 'rxjs';
 import { AuthService } from '../../services/auth.service';
 import { CurrentUserInterface } from 'src/app/shared/types/currentUser.interface';
 import { HttpErrorResponse } from '@angular/common/http';
 import { PersistanceService } from 'src/app/shared/services/persistance.service';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class RegisterEffect {
   constructor(
     private actions$: Actions,
     private readonly authService: AuthService,
-    private readonly persistanceService: PersistanceService
+    private readonly persistanceService: PersistanceService,
+    private readonly router: Router
   ) {}
 
   register$ = createEffect(() =>
@@ -38,5 +40,14 @@ export class RegisterEffect {
         );
       })
     )
+  );
+
+  redirectAfterSubmit$ = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType(registerSuccessAction),
+        tap(() => this.router.navigateByUrl('/'))
+      ),
+    { dispatch: false }
   );
 }
