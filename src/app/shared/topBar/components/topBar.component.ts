9afdcb7eb63424797c183048c5722bfd32aa7e7f
Marcelo L Silva <marcelo.lsilva@abcbrasil.com.br>
2022-07-21 18:26:11 -0300

    feat: add control to topbar

--- src/app/shared/topBar/components/topBar.component.ts
+++ src/app/shared/topBar/components/topBar.component.ts
@@ -1,8 +1,24 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { select, Store } from "@ngrx/store";
+import { Observable } from "rxjs";
+import { currentUserSelector, isAnonymousSelector, isLoggedInSelector } from "src/app/auth/store/selectors";
+import { CurrentUserInterface } from "../../types/currentUser.interface";
 
 @Component({
     selector: "mc-topbar",
     templateUrl: './topBar.component.html',
     styleUrls: ['./topBar.component.css']
 })
-export class TopBarComponent{}
+export class TopBarComponent implements OnInit {
+    isLoggedIn$: Observable<boolean>
+    isAnonymous$: Observable<boolean>
+    currentUser$: Observable<CurrentUserInterface | null>
+
+    constructor(private store: Store) {}
+
+    ngOnInit(): void {
+        this.isLoggedIn$ = this.store.pipe(select(isLoggedInSelector));
+        this.isAnonymous$ = this.store.pipe(select(isAnonymousSelector));
+        this.currentUser$ = this.store.pipe(select(currentUserSelector));
+    }
+}
