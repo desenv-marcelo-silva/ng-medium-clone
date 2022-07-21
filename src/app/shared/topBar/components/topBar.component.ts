89e5d6f7eb3f7955b61fccbc48c70443e439a27e
Marcelo L Silva <marcelo.lsilva@abcbrasil.com.br>
2022-07-21 18:08:42 -0300

    feat(topBar): new component

---
+++ src/app/shared/topBar/components/topBar.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "mc-topbar",
+    templateUrl: './topBar.component.html',
+    styleUrls: ['./topBar.component.css']
+})
+export class TopBarComponent{}
